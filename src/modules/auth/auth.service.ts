bf31fe28d2b10e776a49edc43a0eaac88b7685d1
Mostak Ahamed Nishat <mostakahamed484@gmail.com>
2025-10-25 14:33:37 +0600

    feat: created referral record linking referrer and referee during registration added

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { User } from './auth.model';
 import { TRegisterInput, TLoginInput, TAuthResponse } from './auth.interface';
 import { JwtUtil, ApiError, ReferralCodeUtil } from '../../utils';
 import { StatusCodes } from 'http-status-codes';
+import { ReferralServices } from '../referral/referral.service';
 
 //unique referral code checker
 const generateUniqueReferralCode = async (): Promise<string> => {
@@ -20,20 +21,43 @@ const registerUserIntoDB = async (
   const existing = await User.findOne({ email: payload.email });
   if (existing)
     throw new ApiError(StatusCodes.CONFLICT, 'Email already registered');
+
+  //referral code
   const referralCode = await generateUniqueReferralCode();
+
+  // check if referred by someone
+  let referredById: string | undefined = undefined;
+
+  if (payload.referralCode) {
+    const referrer = await User.findOne({
+      referral_code: payload.referralCode,
+    });
+    if (!referrer) {
+      throw new ApiError(StatusCodes.NOT_FOUND, 'Invalid referral code');
+    }
+    referredById = referrer._id.toString();
+  }
 
   const user = await User.create({
     email: payload.email,
     password_hash: payload.password,
     name: payload.name,
     referral_code: referralCode,
+    referred_by: referredById,
   });
+
+  //  referral record
+  if (referredById) {
+    await ReferralServices.createReferralIntoDB(
+      referredById,
+      user._id.toString()
+    );
+  }
 
   const accessToken = JwtUtil.generateAccessToken({
     id: user._id.toString(),
     email: user.email,
   });
-
   const refreshToken = JwtUtil.generateRefreshToken({
     id: user._id.toString(),
     email: user.email,
